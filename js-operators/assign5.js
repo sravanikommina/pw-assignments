ee7f5b23172ae2dc5eedb0447edbc50946321170
sravanikommina <152785819+sravanikommina@users.noreply.github.com>
2025-01-29 07:24:56 +0530

    adding js-operators assignment

---
+++ js-operators/assign5.js
@@ -0,0 +1,5 @@
+const principle = 10000
+const rate = 2
+let time = 2
+let ans = (principle * rate * time) / 100
+console.log(ans);
---
+++ js-operators/assign6.js
@@ -0,0 +1,4 @@
+const weight = 10
+const height = 5
+const ans = (weight) / (height * height);
+console.log(ans);
